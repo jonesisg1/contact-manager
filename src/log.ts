168df589e0919d1c4a92f3cc45601b170b526092
Ian Jones <jonesi_mail@yahoo.co.uk>
2017-09-05 18:38:02 +0100

    Update grid spacing on form.

--- src/log.ts
+++ src/log.ts
@@ -1,2 +1,6 @@
 import {LogManager} from "aurelia-framework";
+
+//const fse = require('fs-extra');
+import * as fse from 'fs-extra';  //Future project output logging to file.
+
 export var log = LogManager.getLogger('cnt-mgr');
